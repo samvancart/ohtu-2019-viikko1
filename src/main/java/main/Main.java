ab83361d241cea61898293f10c3e67df3673583d
samvancart <sam02cart@gmail.com>
2019-11-11 16:48:04 +0200

    fix

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 1; i++) {
-            System.out.println("virhe");
-        }
+        System.out.println("Hello World");
     }
 }
